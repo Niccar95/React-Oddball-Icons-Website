30ccfbd21e5508974b6a432cc4085f757311b069
Niccar95 <niccar.95@outlook.com>
2025-06-04 21:01:41 +0200

    working on home page layout and styling

--- src/components/IconList.tsx
+++ src/components/IconList.tsx
@@ -1,11 +1,72 @@
-import { Avatar, AvatarAlt, MagnifyingGlass } from "react-oddball-icons";
+import {
+  Avatar,
+  AvatarAlt,
+  Calculator,
+  Calender,
+  Clock,
+  Document,
+  Envelope,
+  Lock,
+  MagnifyingGlass,
+  Presentation,
+  SpeechBubble,
+  SpeechBubbleAlt,
+  StapleDiagram,
+  StapleDiagramAlt,
+  Suitcase,
+} from "react-oddball-icons";
 
 const IconList = () => {
   return (
-    <div className="grid grid-cols-4">
-      <Avatar width={70} height={70} />
-      <AvatarAlt width={70} height={70} />
-      <MagnifyingGlass width={70} height={70} />
+    <div className="h-fit grid grid-cols-4 md:grid-cols-8 gap-3 auto-rows">
+      <div className="rounded-md shadow-sm shadow-black/20 p-2">
+        <Avatar width={60} height={60} />
+      </div>
+      <div className="rounded-md shadow-sm shadow-black/20 p-2">
+        <AvatarAlt width={60} height={60} />
+      </div>
+      <div className="rounded-md shadow-sm shadow-black/20 p-2">
+        <Calculator width={60} height={60} />
+      </div>
+      <div className="rounded-md shadow-sm shadow-black/20 p-2">
+        <Calender width={60} height={60} />
+      </div>
+      <div className="rounded-md shadow-sm shadow-black/20 p-2">
+        <Clock width={60} height={60} />
+      </div>
+      <div className="rounded-md shadow-sm shadow-black/20 p-2">
+        <Document width={60} height={60} />
+      </div>
+      <div className="rounded-md shadow-sm shadow-black/20 p-2">
+        <Envelope width={60} height={60} />
+      </div>
+      <div className="rounded-md shadow-sm shadow-black/20 p-2">
+        <Lock width={60} height={60} />
+      </div>
+      <div className="rounded-md shadow-sm shadow-black/20 p-2">
+        <MagnifyingGlass width={60} height={60} />
+      </div>
+      <div className="rounded-md shadow-sm shadow-black/20 p-2">
+        <Presentation width={60} height={60} />
+      </div>
+      <div className="rounded-md shadow-sm shadow-black/20 p-2">
+        <SpeechBubble width={60} height={60} />
+      </div>
+      <div className="rounded-md shadow-sm shadow-black/20 p-2">
+        <SpeechBubbleAlt width={60} height={60} />
+      </div>
+      <div className="rounded-md shadow-sm shadow-black/20 p-2">
+        <StapleDiagram width={60} height={60} />
+      </div>
+      <div className="rounded-md shadow-sm shadow-black/20 p-2">
+        <StapleDiagram width={60} height={60} />
+      </div>
+      <div className="rounded-md shadow-sm shadow-black/20 p-2">
+        <StapleDiagramAlt width={60} height={60} />
+      </div>
+      <div className="rounded-md shadow-sm shadow-black/20 p-2">
+        <Suitcase width={60} height={60} />
+      </div>
     </div>
   );
 };
--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,19 +1,29 @@
 import { Outlet } from "react-router-dom";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { tomorrow } from "react-syntax-highlighter/dist/esm/styles/prism";
 
 const Layout = () => {
+  const codeString = `npm install react-oddball-icons`;
   return (
     <>
-      <header className="flex justify-around items-center h-[50px]">
-        <h2>React Oddball Logos</h2>
-        <nav className="flex w-1/2 justify-end">
-          <ul className="flex gap-[3rem]">
+      <header className="flex justify-around items-center h-[60px]">
+        <h2>React Oddball Icons</h2>
+        <nav className="flex w-1/2">
+          <ul className="flex w-full justify-between md:justify-end md:gap-[3rem]">
             <li>Home</li>
             <li>Docs</li>
             <li>About</li>
           </ul>
         </nav>
       </header>
-      <main className="flex">
+      <main className="flex flex-1 flex-col gap-8 justify-center items-center">
+        <SyntaxHighlighter
+          language="bash"
+          style={tomorrow}
+          className="rounded-lg p-4"
+        >
+          {codeString}
+        </SyntaxHighlighter>
         <Outlet />
       </main>
       <footer>
