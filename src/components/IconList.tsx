1e5522904542cb2f555d281af278d2f369830402
Niccar95 <niccar.95@outlook.com>
2025-06-05 12:09:15 +0200

    added more styling to icon containers

--- src/components/IconList.tsx
+++ src/components/IconList.tsx
@@ -16,57 +16,36 @@ import {
   Suitcase,
 } from "react-oddball-icons";
 
+const iconComponents = [
+  Avatar,
+  AvatarAlt,
+  Calculator,
+  Calender,
+  Clock,
+  Document,
+  Envelope,
+  Lock,
+  MagnifyingGlass,
+  Presentation,
+  SpeechBubble,
+  SpeechBubbleAlt,
+  StapleDiagram,
+  StapleDiagram,
+  StapleDiagramAlt,
+  Suitcase,
+];
+
 const IconList = () => {
   return (
     <div className="h-fit grid grid-cols-4 md:grid-cols-8 gap-3 auto-rows">
-      <div className="rounded-md shadow-sm shadow-black/20 p-2">
-        <Avatar width={60} height={60} />
-      </div>
-      <div className="rounded-md shadow-sm shadow-black/20 p-2">
-        <AvatarAlt width={60} height={60} />
-      </div>
-      <div className="rounded-md shadow-sm shadow-black/20 p-2">
-        <Calculator width={60} height={60} />
-      </div>
-      <div className="rounded-md shadow-sm shadow-black/20 p-2">
-        <Calender width={60} height={60} />
-      </div>
-      <div className="rounded-md shadow-sm shadow-black/20 p-2">
-        <Clock width={60} height={60} />
-      </div>
-      <div className="rounded-md shadow-sm shadow-black/20 p-2">
-        <Document width={60} height={60} />
-      </div>
-      <div className="rounded-md shadow-sm shadow-black/20 p-2">
-        <Envelope width={60} height={60} />
-      </div>
-      <div className="rounded-md shadow-sm shadow-black/20 p-2">
-        <Lock width={60} height={60} />
-      </div>
-      <div className="rounded-md shadow-sm shadow-black/20 p-2">
-        <MagnifyingGlass width={60} height={60} />
-      </div>
-      <div className="rounded-md shadow-sm shadow-black/20 p-2">
-        <Presentation width={60} height={60} />
-      </div>
-      <div className="rounded-md shadow-sm shadow-black/20 p-2">
-        <SpeechBubble width={60} height={60} />
-      </div>
-      <div className="rounded-md shadow-sm shadow-black/20 p-2">
-        <SpeechBubbleAlt width={60} height={60} />
-      </div>
-      <div className="rounded-md shadow-sm shadow-black/20 p-2">
-        <StapleDiagram width={60} height={60} />
-      </div>
-      <div className="rounded-md shadow-sm shadow-black/20 p-2">
-        <StapleDiagram width={60} height={60} />
-      </div>
-      <div className="rounded-md shadow-sm shadow-black/20 p-2">
-        <StapleDiagramAlt width={60} height={60} />
-      </div>
-      <div className="rounded-md shadow-sm shadow-black/20 p-2">
-        <Suitcase width={60} height={60} />
-      </div>
+      {iconComponents.map((Icon, index) => (
+        <div
+          key={index}
+          className="rounded-md p-2 border-[0.5px] border-neutral-200 bg-gradient-to-br from-white to-neutral-50 hover:from-neutral-50 cursor-pointer hover:to-neutral-100 transition-colors duration-200 shadow-sm shadow-black/10"
+        >
+          <Icon width={60} height={60} />
+        </div>
+      ))}
     </div>
   );
 };
