e225519056d4ed0738ad07c7bcf94f484c85c66c
Niccar95 <niccar.95@outlook.com>
2025-11-03 13:15:40 +0100

    patched spelling error in component

--- src/pages/News.tsx
+++ src/pages/News.tsx
@@ -4,8 +4,8 @@ const News = () => {
   return (
     <section className="flex flex-1 flex-col gap-8 justify-center items-center pt-12 pb-12 pl-4 pr-4">
       <ul>
-        {newsData.map((item) => (
-          <li key={item.id} className="border-b border-gray-200 pb-4">
+        {[...newsData].reverse().map((item) => (
+          <li key={item.id} className="border-b border-gray-200 pb-4 mb-8">
             <p className="text-gray-500 text-sm">{item.date}</p>
             <h2 className="font-semibold">
               {item.title}{" "}
